a87fcb49758136f9f2557c62bdea850ebf5993e4
khablo1508 <khablo.anna@gmail.com>
2022-12-20 20:13:26 +0100

    set up routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Landing from './pages/Landing';
 
 function App() {
   return (
-    <div>
-      <Landing />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<div>Dashboard</div>} />
+        <Route path='/register' element={<div>Register</div>} />
+        <Route path='/landing' element={<Landing />} />
+        <Route path='*' element={<div>Error</div>} />
+      </Routes>
+    </Router>
   );
 }
 
