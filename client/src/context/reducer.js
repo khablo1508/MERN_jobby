d280ac1a477f659037e5c1ecad26b6d3418d427f
khablo1508 <khablo.anna@gmail.com>
2023-01-07 12:31:51 +0300

    Appcontext

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -11,4 +11,14 @@ const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
   const [state, setState] = useState(initialState);
+
+  return (
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+  );
 };
+
+const useAppContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppProvider, initialState, useAppContext };
