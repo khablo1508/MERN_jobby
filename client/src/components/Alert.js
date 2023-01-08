9323a9a91ea4adc645f66818a40995cc1c32358a
khablo1508 <khablo.anna@gmail.com>
2023-01-08 20:41:02 +0100

    add displayAlert function for dynamical chnaging of alert, add dispatch action for displaying alert

--- client/src/components/Alert.js
+++ client/src/components/Alert.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useAppContext } from '../context/appContext';
 
 function Alert() {
-  return <div className='alert alert-danger'>Alert here</div>;
+  const { alertType, alertText } = useAppContext();
+  return <div className={`alert alert-${alertType}`}>{alertText}</div>;
 }
 
 export default Alert;
