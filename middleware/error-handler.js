6d1dec95b5c94313b5768b264ba77db2fa0030f2
khablo1508 <khablo.anna@gmail.com>
2023-01-17 15:10:39 +0100

    handle errors and status codes with npm packages

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,12 @@
+import { StatusCodes } from 'http-status-codes';
+
 const errorHandlerMiddleware = (error, req, res, next) => {
   console.log(error);
-  res.status(500).json({ msg: err });
+  const defaultError = {
+    statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: 'Something went wrong, try again later',
+  };
+  res.status(defaultError.statusCode).json({ msg: error });
 };
 
 export default errorHandlerMiddleware;
