bf45c6cf84b7e962045b4b8a16d9421cb983713f
khablo1508 <khablo.anna@gmail.com>
2023-01-25 22:39:12 +0100

    create web token

--- models/User.js
+++ models/User.js
@@ -24,6 +24,7 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide password'],
     minlength: 6,
+    select: false,
   },
   lastName: {
     type: String,
@@ -47,7 +48,9 @@ UserSchema.pre('save', async function () {
 
 //
 UserSchema.methods.createJWT = function () {
-  return jwt.sign({ userId: this._id }, 'jwtSecret', { expiresIn: '1d' });
+  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
 };
 
 export default mongoose.model('User', UserSchema);
