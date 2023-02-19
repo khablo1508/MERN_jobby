5af90d79f6f0bc6a7d94f7fca409bd16e4e00ed6
khablo1508 <khablo.anna@gmail.com>
2023-02-19 23:20:37 +0100

    setup profile page(client side)

--- client/src/pages/dashboard/Profile.js
+++ client/src/pages/dashboard/Profile.js
@@ -1,7 +1,72 @@
-import React from 'react';
+import { useState } from 'react';
+import { FormRow, Alert } from '../../components';
+import { useAppContext } from '../../context/appContext';
+import DashboardFormWrapper from '../../assets/wrappers/DashboardFormPage';
 
 function Profile() {
-  return <h1>Profile Page</h1>;
+  const { user, showAlert, displayAlert, updateUser, isLoading } =
+    useAppContext();
+  const [name, setName] = useState(user?.name);
+  const [email, setEmail] = useState(user?.email);
+  const [lastName, setLastName] = useState(user?.lastName);
+  const [location, setLocation] = useState(user?.location);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // remove while testing
+    if (!name || !email || !lastName || !location) {
+      displayAlert();
+      return;
+    }
+    console.log({ name, email, lastName, location });
+  };
+
+  return (
+    <DashboardFormWrapper>
+      <form className='form' onSubmit={handleSubmit}>
+        <h3>Profile</h3>
+        {showAlert && <Alert />}
+        <div className='form-center'>
+          <FormRow
+            type='text'
+            name='name'
+            value={name}
+            handleChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          <FormRow
+            type='text'
+            labelText='last name'
+            name='lastName'
+            value={lastName}
+            handleChange={(e) => {
+              setLastName(e.target.value);
+            }}
+          />
+          <FormRow
+            type='email'
+            name='email'
+            value={email}
+            handleChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <FormRow
+            type='text'
+            name='location'
+            value={location}
+            handleChange={(e) => {
+              setLocation(e.target.value);
+            }}
+          />
+          <button className='btn btn-block' type='submit' disabled={isLoading}>
+            {isLoading ? 'Please wait...' : 'Save changes'}
+          </button>
+        </div>
+      </form>
+    </DashboardFormWrapper>
+  );
 }
 
 export default Profile;
